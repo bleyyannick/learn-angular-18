e76f4d296f80bd875f06185e2bab243a84431cf5
Yannick Bley <bleyyannickpro@gmail.com>
2024-06-30 13:25:33 +0200

    feat: sharing data between components and creat housing service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,38 @@
-import { Component } from '@angular/core';
-
+import {Component, inject} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HousingLocationComponent} from '../housing-location/housing-location.component';
+import {HousingService} from '../housing.service';
+import { HousingLocation } from '../housing-location';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule, 
+    HousingLocationComponent],
   template: `
-   <section>
+    <section>
       <form>
         <input type="text" placeholder="Filter by city" />
         <button class="primary" type="button">Search</button>
       </form>
-    </section>`,
-  styleUrl: './home.component.css'
+    </section>
+    <section class="results">
+      <app-housing-location
+        *ngFor="let housingLoc of housingLocationList"
+        [housingLocation]="housingLoc"
+      ></app-housing-location>
+    </section>
+  `,
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  housingService: HousingService = inject(HousingService);
+  housingLocationList: HousingLocation[];
+  readonly baseUrl = 'https://angular.dev/assets/tutorials/common';
+
+  constructor() {
+    this.housingLocationList = this.housingService.getHousingLocations();
+  }
+  
 
 }
